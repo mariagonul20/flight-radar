6efaca1b489045c33bc20a80588e8f3721179477
mariagonul20 <meryemgonulaslan@gmail.com>
2025-11-25 18:58:27 +0300

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const params = {
+  bl_lat: "34.47196",
+  bl_lng: "23.313796",
+  tr_lat: "43.618345",
+  tr_lng: "51.586189",
+  limit: "300",
+  speed: "40,999999",
+  altitude: "10,999999",
+};
---
+++ src/utils/helpers.js
@@ -0,0 +1,26 @@
+import { divIcon } from "leaflet";
+
+export const getIcon = (direction, isActive, isDetailMode) => {
+  return divIcon({
+    html: `
+        <div style="transform: rotate(${direction - 45}deg)">
+            <img src="/plane.svg" width="30px" height="30px"/>
+        </div>
+        `,
+    iconSize: [30, 30],
+    className: `marker ${isDetailMode && "passive-flight"} ${
+      isActive && "active-flight"
+    }`,
+  });
+};
+
+export const formatDate = (unixTime) => {
+  if (!unixTime || unixTime === 0) return null;
+
+  const formatted = new Date(unixTime * 1000);
+
+  return formatted.toLocaleTimeString("tr", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
